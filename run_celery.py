d4493b538cee4a1f120986ec74fa6c850ba605b5
Dave J <dave.jones@bitzesty.com>
2021-03-04 17:41:50 +0000

    📈 Add Sentry error tracking to Celery
    This commit adds Sentry error tracking to our Celery worker app,
    improving our visibility of errors that arise within Celery or the jobs
    running through it.

--- run_celery.py
+++ run_celery.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
+import sentry_sdk
 
 from flask import Flask
 
 # notify_celery is referenced from manifest_delivery_base.yml, and cannot be removed
 from app import notify_celery, create_app  # noqa
+from sentry_sdk.integrations.celery import CeleryIntegration
+
+sentry_sdk.init(
+    integrations=[CeleryIntegration()],
+    traces_sample_rate=1.0
+)
 
 
 application = Flask('delivery')
