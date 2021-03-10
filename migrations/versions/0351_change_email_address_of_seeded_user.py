7f87fc39006509f3bf324aec9df0e2d1445f0b1b
Dave J <dave.jones@bitzesty.com>
2021-03-10 12:56:28 +0000

    🌱 Update default user email to support@domain
    This commit updates the default seeded user to have an email address of
    support@domain, where `domain` is read from the `NOTIFY_EMAIL_DOMAIN`
    environment variable.
    
    Updating the user's email address to an inbox under the admin's control
    allows for a password reset request to be received and actioned.

---
+++ migrations/versions/0351_change_email_address_of_seeded_user.py
@@ -0,0 +1,21 @@
+"""
+
+Revision ID: 428551b7cf6f
+Revises: e17533d246ab
+Create Date: 2021-03-10 12:45:08.022940
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+import os
+
+revision = '428551b7cf6f'
+down_revision = 'e17533d246ab'
+
+
+def upgrade():
+    op.execute("update users set email_address = '{}' where id = '6af522d0-2915-4e52-83a3-3690455a5fe6'".format('support@'+os.environ.get('NOTIFY_EMAIL_DOMAIN')))
+
+def downgrade():
+    op.execute("update users set email_address = 'notify-service-user@digital.cabinet-office.gov.uk' where id = '6af522d0-2915-4e52-83a3-3690455a5fe6'")
