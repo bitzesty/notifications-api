4b037bfc6341ff2c9d85a666be13c15d228d01f5
Dave J <dave.jones@bitzesty.com>
2021-03-10 15:15:15 +0000

    🐛 Fix DB seeding if environment variable unset
    In a previous commit we added a new database migration to set the email
    address of the seeded user based on the `NOTIFY_EMAIL_DOMAIN`
    environment variable.
    
    Unfortunately, in our CI environment, the `NOTIFY_EMAIL_DOMAIN` variable
    in unset, hence the migration process fails.
    
    This commit adds a default domain of "example.com" if the
    `NOTIFY_EMAIL_DOMAIN` variable hasn't been set.

--- migrations/versions/0351_change_email_address_of_seeded_user.py
+++ migrations/versions/0351_change_email_address_of_seeded_user.py
@@ -15,7 +15,8 @@ down_revision = 'e17533d246ab'
 
 
 def upgrade():
-    op.execute("update users set email_address = '{}' where id = '6af522d0-2915-4e52-83a3-3690455a5fe6'".format('support@'+os.environ.get('NOTIFY_EMAIL_DOMAIN')))
+    email_address = "support@" + os.environ.get("NOTIFY_EMAIL_DOMAIN", "example.com")
+    op.execute("update users set email_address = '{}' where id = '6af522d0-2915-4e52-83a3-3690455a5fe6'".format(email_address))
 
 def downgrade():
     op.execute("update users set email_address = 'notify-service-user@digital.cabinet-office.gov.uk' where id = '6af522d0-2915-4e52-83a3-3690455a5fe6'")
