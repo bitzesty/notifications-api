7caaf7ef253c968c38edfb3f8c74d986999ea5f1
Aleksey Zolotov <myxrome@gmail.com>
2021-04-15 00:12:56 +0400

    Enable 'Firetext' sms provider

---
+++ migrations/versions/0357_set_firetext_as_active_provider.py
@@ -0,0 +1,22 @@
+"""
+
+Revision ID: 5235109e7268
+Revises: 07ed5bddc724
+Create Date: 2021-04-14 23:32:09.513520
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+revision = '5235109e7268'
+down_revision = '07ed5bddc724'
+
+
+def upgrade():
+    op.get_bind()
+    op.execute("update provider_details set active = True where identifier = 'firetext'")
+
+def downgrade():
+    op.get_bind()
+    op.execute("update provider_details set active = False where identifier = 'firetext'")
